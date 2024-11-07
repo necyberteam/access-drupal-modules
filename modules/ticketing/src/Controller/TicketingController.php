171d10d9b89746cee1c677023ad2df332894edf5
protitude <miles.france@gmail.com>
2024-11-07 15:41:12 -0700

    ticketing: remove old code - have controller handle logged out users d8-2275

--- modules/ticketing/src/Controller/TicketingController.php
+++ modules/ticketing/src/Controller/TicketingController.php
@@ -4,6 +4,7 @@ namespace Drupal\ticketing\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Routing\TrustedRedirectResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Drupal\Core\Url;
 use Drupal\user\Entity\User;
 
@@ -16,20 +17,29 @@ class TicketingController extends ControllerBase {
    * Redirect to JSM, and prefill the .
    */
   public function doRedirect() {
-    $account = User::load(\Drupal::currentUser()->id());
-    $account_name = $account->getAccountName();
-    $display_name = $account->getDisplayName();
+    if ( User::load(\Drupal::currentUser()->id())->isAuthenticated() ) {
 
-    $uri = Url::fromUri('https://access-ci.atlassian.net/servicedesk/customer/portal/2/group/3/create/17',
-      [
-        'query' => [
-          'customfield_10103' => $account_name,
-          'customfield_10108' => $display_name,
-        ],
-      ]
-    );
+      $account = User::load(\Drupal::currentUser()->id());
+      $account_name = $account->getAccountName();
+      $display_name = $account->getDisplayName();
 
-    return new TrustedRedirectResponse($uri->toString());
+      $uri = Url::fromUri('https://access-ci.atlassian.net/servicedesk/customer/portal/2/group/3/create/17',
+        [
+          'query' => [
+            'customfield_10103' => $account_name,
+            'customfield_10108' => $display_name,
+          ],
+        ]
+      );
+
+      return new TrustedRedirectResponse($uri->toString());
+    }
+    else {
+      $uri = Url::fromRoute('user.login', ['destination' => '/help-ticket']);
+      $response = new RedirectResponse($uri->toString());
+      $response->send();
+    }
+    return;
   }
 
 }
