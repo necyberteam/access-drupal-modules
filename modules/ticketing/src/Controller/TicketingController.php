5d3f499f2909decdd123214c9d0aa80feb41610e
protitude <miles.france@gmail.com>
2024-11-14 09:46:40 -0700

    Switch controller back and replace submit button with link when logged out d8-2275

--- modules/ticketing/src/Controller/TicketingController.php
+++ modules/ticketing/src/Controller/TicketingController.php
@@ -17,29 +17,20 @@ class TicketingController extends ControllerBase {
    * Redirect to JSM, and prefill the .
    */
   public function doRedirect() {
-    if ( User::load(\Drupal::currentUser()->id())->isAuthenticated() ) {
+    $account = User::load(\Drupal::currentUser()->id());
+    $account_name = $account->getAccountName();
+    $display_name = $account->getDisplayName();
 
-      $account = User::load(\Drupal::currentUser()->id());
-      $account_name = $account->getAccountName();
-      $display_name = $account->getDisplayName();
+    $uri = Url::fromUri('https://access-ci.atlassian.net/servicedesk/customer/portal/2/group/3/create/17',
+      [
+        'query' => [
+          'customfield_10103' => $account_name,
+          'customfield_10108' => $display_name,
+        ],
+      ]
+    );
 
-      $uri = Url::fromUri('https://access-ci.atlassian.net/servicedesk/customer/portal/2/group/3/create/17',
-        [
-          'query' => [
-            'customfield_10103' => $account_name,
-            'customfield_10108' => $display_name,
-          ],
-        ]
-      );
-
-      return new TrustedRedirectResponse($uri->toString());
-    }
-    else {
-      $uri = Url::fromRoute('user.login', ['destination' => '/help-ticket']);
-      $response = new RedirectResponse($uri->toString());
-      $response->send();
-    }
-    return;
+    return new TrustedRedirectResponse($uri->toString());
   }
 
 }
