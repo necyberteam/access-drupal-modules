ffd2d42d28b20852f6cb4cbf9f3d07563a9f27fa
protitude <miles.france@gmail.com>
2024-11-16 10:03:50 -0700

    access_misc: fix js to work with ajax

--- modules/access_misc/js/people-directory.js
+++ modules/access_misc/js/people-directory.js
@@ -1,20 +1,28 @@
+(function ($, Drupal, once) {
+  "use strict";
+  Drupal.behaviors.nodeAddTags = {
+    attach: function (context, settings) {
 
-var item = document.querySelectorAll('.user-skills');
-item.forEach(addCount);
-function addCount(item) {
-  // count .square-tag li inside index
-  var squareTags = item.querySelectorAll('a').length;
-  if (squareTags > 5) {
-    var squareTags = squareTags - 5;
-    var more = "+ " + squareTags + " more";
-    // Create new span element
-    var span = document.createElement("span");
-    // Add style.display flex to span.
-    span.style.display = "flex";
-    span.style.alignItems = "center";
-    span.style.fontSize = "14px";
-    // Add more text to span.
-    span.innerHTML = more;
-    item.appendChild(span);
-  }
-}
+      var item = document.querySelectorAll('.user-skills');
+      item.forEach(addCount);
+      function addCount(item) {
+        // count .square-tag li inside index
+        var squareTags = item.querySelectorAll('a').length;
+        if (squareTags > 5 && item.querySelector('span') == null) {
+          var squareTags = squareTags - 5;
+          var more = "+ " + squareTags + " more";
+          // Create new span element
+          var span = document.createElement("span");
+          // Add style.display flex to span.
+          span.style.display = "flex";
+          span.style.alignItems = "center";
+          span.style.fontSize = "14px";
+          // Add more text to span.
+          span.innerHTML = more;
+          item.appendChild(span);
+        }
+      }
+
+    }
+  };
+})(jQuery, Drupal, once);
