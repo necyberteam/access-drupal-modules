5794ff24692f89d0299d9d4eb3dd56c09d2a246e
protitude <miles.france@gmail.com>
2024-12-10 12:28:15 -0700

    access_misc: d8-2211 - set and hide items on registration form - Add events searchApi no show boolean

---
+++ modules/access_misc/src/Plugin/search_api/processor/EventNoShow.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\access_misc\Plugin\search_api\processor;
+
+use Drupal\search_api\Datasource\DatasourceInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\search_api\Processor\ProcessorProperty;
+
+/**
+ * Search API Processor for indexing Event type as the built in one isn't working..
+ *
+ * @SearchApiProcessor(
+ *   id = "custom_event_no_show",
+ *   label = @Translation("Custom Event No Show"),
+ *   description = @Translation("Custom Event No Show Boolean Processor"),
+ *   stages = {
+ *     "add_properties" = 0,
+ *   },
+ *   locked = true,
+ *   hidden = true,
+ * )
+ */
+class EventNoShow extends ProcessorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions(DatasourceInterface $datasource = NULL) {
+    $properties = [];
+
+    if (!$datasource) {
+      $definition = [
+        'label' => $this->t('Event No Show'),
+        'description' => $this->t('The event no show type.'),
+        'type' => 'string',
+        'processor_id' => $this->getPluginId(),
+      ];
+      $properties['search_api_custom_event_no_show'] = new ProcessorProperty($definition);
+
+    }
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+    $entity = $item->getOriginalObject()->getValue();
+
+    $fields = $item->getFields();
+    $fields = $this->getFieldsHelper()
+      ->filterForPropertyPath($fields, NULL, 'search_api_custom_event_no_show');
+    foreach ($fields as $field) {
+      $series = $entity->getEventSeries();
+      if (empty($series)) {
+        return;
+      }
+
+      $no_show = $series->get('field_event_no_listing')->getValue();
+
+      if ($no_show == NULL) {
+        $field->addValue(0);
+      } else {
+        $field->addValue(1);
+      }
+
+    }
+  }
+
+}
