2c41f7fcc2903ed62b06bf1adedb54e29396d6da
protitude <miles.france@gmail.com>
2025-01-10 13:38:37 -0700

    access_misc: adding in initial email for registration

---
+++ modules/access_misc/src/Plugin/EmailBuilder/RegistrationEmailBuilder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\access_misc\Plugin\EmailBuilder;
+
+use Drupal\symfony_mailer\Processor\EmailBuilderBase;
+use Drupal\symfony_mailer\EmailInterface;
+
+/**
+ * Email Builder plug-in for the access_misc module.
+ *
+ * @EmailBuilder(
+ *   id = "access_misc",
+ *   sub_types = {
+ *     "register" = @Translation("User Registers for an Event"),
+ *   },
+ *   common_adjusters = {"email_subject", "email_body"},
+ * )
+ */
+class RegistrationEmailBuilder extends EmailBuilderBase {
+  public function build(EmailInterface $email) {
+    $email->setFrom('noreply@support.access-ci.org');
+  }
+}
